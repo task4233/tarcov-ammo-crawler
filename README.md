fc27a0a689e57aaf4f33ad498d05be4a94fdbbcf
task4233 <tks.m1205@gmail.com>
2021-02-26 20:31:11 +0900

    add try-catch
    close #2

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ## QuickStart
 ```
 python3 -m pip install -r requirements.txt
+python3 scrape.py
 ```
--- scrape.py
+++ scrape.py
@@ -19,28 +19,33 @@ class ScrapeTarcov():
         res = ""
         try:
             res = requests.get(ammo_url)
-        except Exception: # as e
-            # print(e)
+        except Exception as e:
+            print(e)
             return None
 
         soup = BeautifulSoup(res.text, "html.parser")
         # h3が弾の詳細が書かれている部分
         lists = soup.find_all("h3")
         for list in lists:
-            # 弾薬の選び方は無視する
-            if "弾薬" in list:
-                continue
-            ammos = list.find_next().find_next().find_next().find_next().select_one("table > tbody > tr > td:nth-child(2)")
-            # 見出しに弾が入っていない物は除去
+            try:
+                ammos = list.find_next().find_next().find_next().find_next().select_one("table > tbody > tr > td:nth-child(2)")
+                # 見出しに弾が入っていない物は除去
+            except Exception as e:
+                print(e)
+                return None
             if ammos == None:
                 continue
             ammo_name = list.next_element
             print(ammo_name)
-            with open("out/" + ammo_name + ".csv", "w") as f:
-                if ammos != None:
-                    ammos_a = ammos.find_all('a')
-                    writer = csv.writer(f)
-                    writer.writerows(ammos_a)
+            try:
+                with open("out/" + ammo_name + ".csv", "w") as f:
+                    if ammos != None:
+                        ammos_a = ammos.find_all('a')
+                        writer = csv.writer(f)
+                        writer.writerows(ammos_a)
+            except Exception as e:
+                print(e)
+                return None
         return res.text
 
 
